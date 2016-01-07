1764268e2e10252b7d0ca836b332604f66abc247
Wiktor Stadnik <widzir92@gmail.com>
2016-01-07 22:33:54 +0000

    prepare base template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,35 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
-console.log('test');
+var starbucksMap = {
+    mapSelector: '#map-canvas',
+    
+    start: {
+        lat: 52.200,
+        lng: 19.134,
+        zoom: 6
+    },
+    map: {},
+     markers: [],
+    
+    init: function() {
+        this.map = new google.maps.Map(jQuery(this.mapSelector).get(0), { // google.maps needs DOM not a jQuery object
+            zoom: this.start.zoom,
+            center: this.start
+        });
+        this.addMarker();
+    },
+    setMarkers: function(){
+        //get all markers form ajax
+    },
+    addMarker: function(){
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(this.start.lat, this.start.lng),
+            map: this.map,
+            title: 'test'
+        });
+        this.markers.push(marker);
+    }
+    
+    
+}
+jQuery(document).ready(function(){
+   starbucksMap.init(); 
+});
